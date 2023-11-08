9318810de36d47b1dfeecd0b4899bc1a34e5ea7e
Kshitij0169 <tiwari.ks@northeastern.edu>
2023-11-08 15:57:44 -0500

    error fixes for deployment

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -25,7 +25,7 @@ const LeftSidebar = () => {
 
           return (
             <Link
-              href={link.route}
+              href={{ pathname: `${link.route}` }}
               key={link.label}
               className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}
             >
@@ -44,6 +44,7 @@ const LeftSidebar = () => {
 
       <div className="mt-10 px-6">
         <SignedIn>
+          {/* @ts-ignore */}
           <SignOutButton signOutCallback={() => router.push("/sign-in")}>
             <div className="flex cursor-pointer gap-4 p-4">
               <Image
